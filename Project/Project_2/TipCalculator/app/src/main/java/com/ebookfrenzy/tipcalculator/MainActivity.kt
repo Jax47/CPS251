1d00e875b8e2b3a6fa824b28a2f0fc7545cc8105
Joshua Anderson <joshuaanderson@Joshuas-MBP.attlocal.net>
2022-10-09 22:28:48 -0400

    Added some comment for future me

--- Project/Project_2/TipCalculator/app/src/main/java/com/ebookfrenzy/tipcalculator/MainActivity.kt
+++ Project/Project_2/TipCalculator/app/src/main/java/com/ebookfrenzy/tipcalculator/MainActivity.kt
@@ -19,21 +19,29 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("SetTextI18n")
     fun calculate(view: View){
+        /*Reminder: In order to use the onClick attribute the function
+         being call must take the view object in as a argument.
+         */
 
         val billCost = binding.billAmount.text.toString()
 
         if(billCost.isNotEmpty()){
+            /*This will convert the string into a BigDecimal object if
+            the string itself is the appropriate type ie a number/digit
+            otherwise it return a null object
+
+             */
             val result = billCost.toBigDecimalOrNull()
+
             if(result != null){
-                //TODO implement conversion here
 
                 //Get in habit of using big decimal with currency to preserve accuracy
-
                 var tip1 = result.multiply(".10".toBigDecimal())
                 var tip2 = result.multiply(".15".toBigDecimal())
                 var tip3 = result.multiply(".20".toBigDecimal())
 
-
+                //Check if there is a better way to do the task bellow
+                    //Task:  Calculate the result and outputting it to the corresponding view
                 binding.viewTipAmount.text = "The tip are as follow:\n" +
                         "\n" +
                         "10% = " + (result + tip1).toString() + "\n" +
